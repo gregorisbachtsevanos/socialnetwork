b04b355af1db74b2b93d719f29d27ab384a9829e
GregorisB <gregorbachts@gmail.com>
2021-11-15 22:38:36 +0200

    responsive

--- public/includes/header.php
+++ public/includes/header.php
@@ -15,5 +15,6 @@
 	<meta charset="utf-8">
 	<title>Login</title>
 	<link rel="stylesheet" href="../styles/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
 </head>
