b4c67fc88327e8dd7a8559a3c44bfc9dc577fae4
GregorisB <94171068+GregorisB@users.noreply.github.com>
2022-02-18 09:19:43 +0200

    chat 0.1

--- chat/server.js
+++ chat/server.js
@@ -1,19 +1,18 @@
+const http = require('http');
 const express = require('express');
 const app = express();
-const http = require('http');
 const { Socket } = require('socket.io');
 const server = http.createServer(app)
+
 const io = require('socket.io')(http)
 const mysql = require('mysql')
 
-const PORT = 3000
+const PORT = 3030
 
-io.on('connection', ()=>{
-	console.log(`User connected ${socket.id}`)
+app.get('', (req, res) => {
+	res.send('Express server')
 })
 
-
-
-server.listen(PORT, ()=>{
+app.listen(PORT, () => {
 	console.log(`Server is listening to port: ${PORT}`)
 })
